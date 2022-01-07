b0c591c12ff1601b58096e035d5cb8b23e4dcac0
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-01-07 15:52:08 +0100

    Update repo

--- repo/installscripts/mbsinstall.lua
+++ repo/installscripts/mbsinstall.lua
@@ -1,2 +1,3 @@
 shell.run("wget https://raw.githubusercontent.com/SquidDev-CC/mbs/master/mbs.lua mbs.lua")
 shell.run("mbs.lua install")
+shell.run("rm mbs.lua")
---
+++ repo/installscripts/mbsremove.lua
@@ -0,0 +1,3 @@
+shell.run("rm mbs.lua")
+shell.run("rm /startup/00_mbs.lua")
+shell.run("rm /.mbs")
